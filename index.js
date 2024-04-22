564198a43ece1c3838df0dc1fcdebfe2efd7f881
cirroskais <cirroskais@madhouselabs.net>
2024-04-22 02:29:34 -0400

    My god

---
+++ index.js
@@ -0,0 +1,27 @@
+import { readdirSync, writeFileSync, cpSync } from "node:fs";
+import emoji_map from "./emoji_map.json";
+const dir = readdirSync("./png");
+
+let output = {};
+
+for (let emojiName of Object.keys(emoji_map)) {
+    let emojiUtfEscape = emoji_map[emojiName];
+    for (let emojiFile of dir) {
+        let x = emojiFile.replace("emoji_u", "").replace(".png", "");
+        let y = x.split("_");
+        let z = [];
+
+        for (let _ of y) {
+            z.push(parseInt(_, 16));
+        }
+
+        let codepoint = String.fromCodePoint(...z);
+        if (codepoint === emojiUtfEscape) {
+            console.log(`Found ${emojiName} (${codepoint})!`);
+            cpSync(`./png/${emojiFile}`, `./emojis/${emojiFile}`);
+            output[emojiName] = `emojis/${emojiFile}`;
+        }
+    }
+}
+
+writeFileSync("map.json", JSON.stringify(output, null, 4));
